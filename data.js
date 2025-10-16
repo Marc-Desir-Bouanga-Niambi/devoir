0f9d732eea4cac9439166630f5752b7fd6bfea8b
Marc Désir Bouanga Niambi <>
2025-10-16 15:02:51 +0200

    j'espère que mon cnflit est créé

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 const books = [
   {
     "title": "Le Petit Prince",
-    "author": "Antoine de Saint-Exupéry"
+    "author": "Antoine de Saint-Exupéry",
+    "genre" : "conflit"
   },
   {
     "title": "1984",
