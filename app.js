23d620fa5d6f9c60f5448c5b1f96b2f80ca50ec9
Marc Désir Bouanga Niambi <>
2025-10-16 14:24:48 +0200

    ajout d'une alerte

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function renderBooks() {
     const editBtn = document.createElement('button');
     editBtn.textContent = "Modifier titre";
     editBtn.onclick = () => {
-      // Insérer ici le code pour modifier le titre du livre
+      alert("Bonjour, ceci est une alerte !");      
     };
     actionsTd.appendChild(editBtn);
 
